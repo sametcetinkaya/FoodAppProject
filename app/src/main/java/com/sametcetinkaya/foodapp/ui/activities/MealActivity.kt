182de610150778199910964facd647b36000a2d2
Samet Çetinkaya <samet.ctnky35@gmail.com>
2023-01-28 23:39:18 +0300

    Design meal activity

---
+++ app/src/main/java/com/sametcetinkaya/foodapp/ui/activities/MealActivity.kt
@@ -0,0 +1,11 @@
+package com.sametcetinkaya.foodapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MealActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal)
+    }
+}
